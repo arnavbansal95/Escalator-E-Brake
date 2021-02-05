1e6a93ef4b752be5262fc3bd201fe07b2e270940
Arnav Bansal <arnav.bansal95@gmail.com>
2021-02-05 16:39:43 +0100

    Added ReadRPM Function

--- cpp/main/src/inputs.cpp
+++ cpp/main/src/inputs.cpp
@@ -26,3 +26,55 @@ uint8_t ReadInput(int Input)
     } 
     return ret;
 }
+
+float ReadRPM(motor_direction_t* dir)
+{
+    static volatile int lastEncoded = 0;
+    static volatile long encoderValue = 0;
+    static volatile long prevEncoderValue = 0;
+    static unsigned long prevTime = 0;
+    static unsigned long currTime = 0;
+    static motor_direction_t dir_var = STOP;
+    static float rpm = 0;
+
+    int MSB = ReadInput(ENCODER_A);                                         //MSB = most significant bit
+    int LSB = ReadInput(ENCODER_B);                                         //LSB = least significant bit
+    int encoded = (MSB << 1) | LSB;                                         //converting the 2 pin value to single number
+    int sum = (lastEncoded << 2) | encoded;                                 //adding it to the previous encoded value
+    if((sum != 0000) && (sum != 1111) && (sum != 0101) && (sum != 1010))
+    {
+        if((sum == 0b1101) || (sum == 0b0100) || (sum == 0b0010) || (sum == 0b1011))
+            encoderValue ++;
+        if((sum == 0b1110) || (sum == 0b0111) || (sum == 0b0001) || (sum == 0b1000))
+            encoderValue --;
+    }
+    lastEncoded = encoded;                                                  //store this value for next time
+
+    currTime = millis();
+    if((currTime - prevTime) > 250)
+    {
+        float diff = EncoderDataDecoder((encoderValue - prevEncoderValue), &dir_var);
+        rpm = (diff / (float)1024) * 240;
+        prevEncoderValue = encoderValue;
+        prevTime = currTime;
+    }
+    *dir = dir_var;
+    return(rpm);
+}
+
+long EncoderDataDecoder(long encodedVal, motor_direction_t* dir)
+{
+    if(encodedVal < 0)
+    {
+        *dir = FWD;
+        return((-1)*encodedVal);
+    }
+    else 
+    {
+        if(encodedVal > 0)
+            *dir = REV;
+        else
+            *dir = STOP;
+        return(encodedVal);
+    }
+}
--- cpp/main/src/inputs.h
+++ cpp/main/src/inputs.h
@@ -8,7 +8,16 @@
 #define ENCODER_A    A0                         //  ENCODER_PIN A (Green)
 #define ENCODER_B    A1                         //  ENCODER_PIN B (White)
 
-void InitInputs(void);                          //  Initialize Inputs
-uint8_t ReadInput(int Input);                   //  Read Inputs
+typedef enum
+{
+    FWD,
+    REV,
+    STOP
+} motor_direction_t;
+
+void InitInputs(void);                                                  //  Initialize Inputs
+uint8_t ReadInput(int Input);                                           //  Read Inputs
+float ReadRPM(motor_direction_t* dir);                                  //  Returns the calculated RPM value from Input Encoder and pass Direction
+long EncoderDataDecoder(long encodedVal, motor_direction_t* dir);       //  Returns the info derived from encoder data
 
 #endif
