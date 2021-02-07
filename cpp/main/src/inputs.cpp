dd57b9d5ec8e67b3e7ba7b839aadc7a9fe9c2354
Arnav Bansal <arnav.bansal95@gmail.com>
2021-02-07 12:08:32 +0100

    Debug RPM - 5

--- cpp/main/src/inputs.cpp
+++ cpp/main/src/inputs.cpp
@@ -34,14 +34,18 @@ float ReadRPM(motor_direction_t* dir)
     static volatile long prevEncoderValue = 0;
     static unsigned long prevTime = 0;
     static unsigned long currTime = 0;
+    static int MSB;
+    static int LSB;
+    static int encoded;
+    static int sum;
     static motor_direction_t dir_var = STOP;
     static float rpm = 0;
 
-    int MSB = ReadInput(ENCODER_A);                                         //MSB = most significant bit
-    int LSB = ReadInput(ENCODER_B);                                         //LSB = least significant bit
-    int encoded = (MSB << 1) | LSB;                                         //converting the 2 pin value to single number
-    int sum = (lastEncoded << 2) | encoded;                                 //adding it to the previous encoded value
-    if((sum != 0000) && (sum != 1111) && (sum != 0101) && (sum != 1010))
+    MSB = ReadInput(ENCODER_A);                                         //MSB = most significant bit
+    LSB = ReadInput(ENCODER_B);                                         //LSB = least significant bit
+    encoded = (MSB << 1) | LSB;                                         //converting the 2 pin value to single number
+    sum = (lastEncoded << 2) | encoded;                                 //adding it to the previous encoded value
+    if((sum != 0b0000) && (sum != 0b1111) && (sum != 0b0101) && (sum != 0b1010))
     {
         if((sum == 0b1101) || (sum == 0b0100) || (sum == 0b0010) || (sum == 0b1011))
             encoderValue ++;
@@ -49,8 +53,9 @@ float ReadRPM(motor_direction_t* dir)
             encoderValue --;
     }
     lastEncoded = encoded;                                                  //store this value for next time
+    Serial.println(sum, BIN);
 
-    currTime = millis();
+    /*currTime = millis();
     if((currTime - prevTime) > 1000)
     {
         long diff = EncoderDataDecoder((encoderValue - prevEncoderValue), &dir_var);
@@ -58,7 +63,7 @@ float ReadRPM(motor_direction_t* dir)
         rpm = ((float)diff / (float)1024) * 60;
         prevEncoderValue = encoderValue;
         prevTime = currTime;
-    }
+    }*/
     *dir = dir_var;
     return(rpm);
 }
